795ebd52e74fb0f5f2db0259b2b24ab519ad0501
CharmingDays <nawhpongmaji1@gmail.com>
2024-04-15 10:46:44 -0500

    updated docker

--- kurusaki/docker.sh
+++ kurusaki/docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #setup the docker images/containers for discord
 sudo docker build -t kurusaki.
-sudo docker network create discord
+sudo docker network create discord #creat network
 cd lavalink
 sudo docker build -t lavalink .
-sudo docker run -d -p 2333:2333/tcp -p 2333:2333/udp --name lavalink --net discord --restart always lavalink
+# sudo docker run -d -p 2333:2333/tcp -p 2333:2333/udp --name lavalink --net discord --restart always lavalink
 sudo docker run -d --name kurusaki --net discord kurusaki
--- update_bot.sh
+++ update_bot.sh
@@ -1,9 +1,13 @@
 # !/bin/bash
 # auto update the bot with the latest github commit and restart the bot service
+sudo docker stop kurusaki
+sudo docker rm kurusaki
+sudo docker rmi kurusaki
 git clone https://github.com/CharmingDays/Kurusaki clone #clone the latest commit
 mv clone/kurusaki kurusaki_copy #move the bot folder to a new folder
 mv kurusaki/.env kurusaki_copy #move the .env file to the new folder
 sudo rm -r clone kurusaki #remove the old bot folder
 mv kurusaki_copy kurusaki #rename the folder back to kurusaki   
 cd kurusaki
-~/kurusaki/docker.sh
+chmod +x docker.sh
+./docker.sh
