62b40c739de58682f51501cccfbb8da1a5b53063
CharmingDays <nawhpongmaji1@gmail.com>
2024-07-24 11:37:06 -0500

    updated dockerfile

--- kurusaki/lavalink/docker.sh
+++ kurusaki/lavalink/docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker build -t lavalink .
-docker run -d -p 2333:2333/tcp -p 2333:2333/udp --name lavalink --restart always lavalink
+sudo docker run -d -p 2333:2333/tcp -p 2333:2333/udp --name lavalink --net discord --restart always -v /home/ubuntu/kurusaki/lavalink:/lavalink_data lavalink
